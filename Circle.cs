c3e47d7b9c8d287283f2e3dc9bd7c18ca0d03f2b
Trucksson <Henric.kurtsson@gmail.com>
2024-11-17 23:20:08 +0100

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_5_Circle
+{
+    internal class Circle
+    {
+        public double Radius { get; set; } 
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double GetArea()
+        {
+            return Radius * Radius * Math.PI; //Genom Pi så kan vi räkna ut cirkelns area r * r * PI
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_5_Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Ange radien på cirkeln :D ");
+                double radie = Convert.ToDouble(Console.ReadLine());
+
+                Circle cirkel = new Circle(radie);  // Skapar en Cirkel
+
+                double area = cirkel.GetArea();  // Beräknar arean
+                Console.WriteLine($"Arean av cirkeln med radie {radie} är: {area}"); // Skriver ut cirkelns Radie och Area
+            }
+            Console.ReadLine(); // Håller konsolen öppen
+        }
+    }
+    }
+
